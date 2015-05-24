ff979fa6d1daf6baf87f5476168c0f45841defc4
Joe <joseph.b.barclay@gmail.com>
2015-05-24 16:53:17 -0400

    Programming Assignment Two Modifications
    These modification complete the requirements for R-Programming
    Programming Assignment two!

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## these functions return the inverse of matrix... it first checks to see if a cache value is stored.
+## otherwise it calculates and returns the cache inverse
 
-## Write a short comment describing this function
+## these are the basic get and set functions for the cache. 
+## it first initializes the inverse (called inv) of matrix x to NULL
+## then it creates helper functions set/get/setinv/getinv to handle assigning and setting the cache
 
 makeCacheMatrix <- function(x = matrix()) {
+  #initialize the inverse to NULL
+  inv <- NULL
+  
+  #again initialze the inverse to NULL and set the matrix to y
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  
+  #return the matrix
+  get <- function() x
+
+  #set the cache inverse value
+  setinv <- function(inverse) {
+    inv <<- inverse
+  }
+  
+  #return the cache inverse value
+  getinv <- function() inv
+  
+  #not sure why we need this but it was in the example. sue me
+  list(set=set, 
+       get=get, 
+       setinv=setinv, 
+       getinv=getinv
+       )
 
 }
 
@@ -12,4 +40,27 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  # get the value of the inverse using the getinv function
+  inv <- x$getinv()
+  
+  # check to see if the value assigned to inv is NULL. 
+  # if it is not the inverse is assigned as return the cache value
+  if (!is.null(inv)){
+    message("inverse is calculated, returning cache data")
+    return(inv)
+  }
+  # inverse is not calculated, do the manual work
+  else{
+    #get the matrix
+    data <- x$get()
+    
+    #assign the inverse using solve function
+    inv <- solve(data, ...)
+    
+    #set cache value as inv
+    x$setinv(inv)
+    
+    return(inv)
+  }
+  
 }
